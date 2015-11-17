422a8c5a44b0b60a245f76d8b585461ac4841af4
SeomGi, Han <iandmyhand@gmail.com>
2015-11-18 08:53:02 +0900

    Add dateUtils.js contains a function that can convert unixtimestamp to formatted date.

---
+++ dateUtils.js
@@ -0,0 +1,24 @@
+var unixTimestampFormat = function(unixTimestamp, formatString) {
+    var date = new Date(unixTimestamp * 1000);
+    function pad(value) {
+        return (value.toString().length < 2) ? '0' + value : value;
+    }
+    return formatString.replace(/%([a-zA-Z])/g, function (_, formatStringCode) {
+        switch (formatStringCode) {
+        case 'Y':
+            return date.getUTCFullYear();
+        case 'M':
+            return pad(date.getUTCMonth() + 1);
+        case 'd':
+            return pad(date.getUTCDate());
+        case 'H':
+            return pad(date.getUTCHours());
+        case 'm':
+            return pad(date.getUTCMinutes());
+        case 's':
+            return pad(date.getUTCSeconds());
+        default:
+            throw new Error('Unsupported format code: ' + formatStringCode);
+        }
+    });
+}
