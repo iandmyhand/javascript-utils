661419a51f528de4a1c985ff742ece5c37e7c0c4
SeomGi, Han <iandmyhand@gmail.com>
2015-12-03 10:53:08 +0900

    Add usages for numberUtils > format function.

--- numberUtils.js
+++ numberUtils.js
@@ -1,7 +1,18 @@
+/**
+ * new Number(0).format() == "0"
+ * new Number(1234).format() == "1,234"
+ * new Number(1234567890).format() == "1,234,567,890"
+ * new Number(-1234567890).format() == "-1,234,567,890"
+ * new Number(1234567890.123456789).format() == "1,234,567,890.1234567"
+ * new Number(-1234567890.123456789).format() == "-1,234,567,890.1234567"
+ *
+ * Also, you can use variable.
+ * var n = 1234567890.123456789; n.format(); == "1,234,567,890.1234567"
+ */
 Number.prototype.format = function(){
-    if(this==0) return 0;
-    var reg = /(^[+-]?\d+)(\d{3})/;
-    var n = (this + '');
+    if(this == 0) return "0";
+    var reg = /(^[+-]?\d+)(\d{3})/,
+        n = (this + '');
     while (reg.test(n)) n = n.replace(reg, '$1' + ',' + '$2');
     return n;
 };
